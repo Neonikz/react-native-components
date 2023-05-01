a7fe735ab3edbb71ac73f60ac797769f8eecb05e
Nicolas Heizenreder <heizenredernicolas@gmail.com>
2023-04-30 22:33:58 -0300

    first commit

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Icon name="star-outline" color="#000" size={50} />
+        </View>
+    )
+}
+
+export default HomeScreen
