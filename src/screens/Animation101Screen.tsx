c578bff140bebef74bcff3176756f663fa08df99
Nicolas Heizenreder <heizenredernicolas@gmail.com>
2023-05-10 22:04:40 -0300

    valueXY animation ready

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,62 +1,49 @@
-import React, { useRef } from 'react'
-import { View, StyleSheet, Animated, Button } from 'react-native';
+import React from 'react';
+import {View, StyleSheet, Animated, Button} from 'react-native';
+import {useAnimation} from '../hooks/useAnimation';
 
 const Animation101Screen = () => {
+  const {opacity, position, fadeIn, fadeOut, startMovingPosition} =
+    useAnimation();
 
-    const opacity = useRef(new Animated.Value(0.4)).current
-
-    const fadeIn = () => {
-        Animated.timing(
-            opacity,
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        style={{
+          ...styles.purpleBox,
+          marginBottom: 20,
+          opacity,
+          transform: [
             {
-                toValue: 1,
-                duration: 3000,
-                useNativeDriver: true
-            }
-        ).start()
-    }
-    const fadeOut = () => {
-        Animated.timing(
-            opacity,
-            {
-                toValue: 0,
-                duration: 300,
-                useNativeDriver: true
-            }
-        ).start()
-    }
+              translateX: position,
+            },
+          ],
+        }}
+      />
 
-    return (
-        <View style={styles.container}>
-            <Animated.View style={{
-                ...styles.purpleBox,
-                marginBottom: 20,
-                opacity
-            }} />
+      <Button
+        title="Fade In"
+        onPress={() => {
+          fadeIn();
+          startMovingPosition(100);
+        }}
+      />
+      <Button title="Fade Out" onPress={fadeOut} />
+    </View>
+  );
+};
 
-            <Button
-                title="Fade In"
-                onPress={fadeIn}
-            />
-            <Button
-                title="Fade Out"
-                onPress={fadeOut}
-            />
-        </View>
-    )
-}
-
-export default Animation101Screen
+export default Animation101Screen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    purpleBox: {
-        backgroundColor: '#5856D6',
-        width: 150,
-        height: 150,
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  purpleBox: {
+    backgroundColor: '#5856D6',
+    width: 150,
+    height: 150,
+  },
 });
--- src/screens/Animation102Screen.tsx
+++ src/screens/Animation102Screen.tsx
@@ -1,20 +1,51 @@
-import React from 'react'
-import { View, StyleSheet } from 'react-native';
+import React, {useRef} from 'react';
+import {View, StyleSheet, Animated, PanResponder} from 'react-native';
 
 const Animation102Screen = () => {
-    return (
-        <View style={{ flex: 1 }}>
-            <View style={styles.purpleBox} />
-        </View>
-    )
-}
+  const pan = useRef(new Animated.ValueXY()).current;
 
-export default Animation102Screen
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event(
+      [
+        null,
+        {
+          dx: pan.x, // x,y are Animated.Value
+          dy: pan.y,
+        },
+      ],
+      {useNativeDriver: false},
+    ),
+    onPanResponderRelease: () => {
+      Animated.spring(
+        pan, // Auto-multiplexed
+        {toValue: {x: 0, y: 0}, useNativeDriver: false}, // Back to zero
+      ).start();
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[pan.getLayout(), styles.purpleBox]}
+      />
+    </View>
+  );
+};
+
+export default Animation102Screen;
 
 const styles = StyleSheet.create({
-    purpleBox: {
-        backgroundColor: '#5856D6',
-        width: 150,
-        height: 150,
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  purpleBox: {
+    backgroundColor: '#75CEDB',
+    width: 150,
+    height: 150,
+    borderRadius: 4,
+  },
 });
