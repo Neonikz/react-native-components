62232e510ffb79440e6c81e104fcdc50e0cf7a0f
Nicolas Heizenreder <heizenredernicolas@gmail.com>
2023-05-15 18:34:45 -0300

    alerts ready

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -4,6 +4,7 @@ import HomeScreen from '../screens/HomeScreen';
 import Animation101Screen from '../screens/Animation101Screen';
 import Animation102Screen from '../screens/Animation102Screen';
 import { SwitchScreen } from '../screens/SwitchScreen';
+import { AlertScreen } from '../screens/AlertScreen';
 
 const Stack = createStackNavigator();
 
@@ -11,13 +12,14 @@ const Navigator = () => {
     return (
         <Stack.Navigator
             screenOptions={{
-                headerShown: false
+                headerShown: false,
             }}
         >
             <Stack.Screen name="HomeScreen" component={HomeScreen} />
             <Stack.Screen name="Animation101Screen" component={Animation101Screen} />
             <Stack.Screen name="Animation102Screen" component={Animation102Screen} />
             <Stack.Screen name="SwitchScreen" component={SwitchScreen} />
+            <Stack.Screen name="AlertScreen" component={AlertScreen} />
         </Stack.Navigator>
     );
 }
